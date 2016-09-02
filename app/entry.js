0051e50cd076cbe33d25693b35fd14e03bfb1f1c
Katherine Beame <katherine.beame@gmail.com>
2016-09-01 23:03:57 -0700

    it never ends

--- app/entry.js
+++ app/entry.js
@@ -1,6 +1,6 @@
 'use strict';
 // require('!!file?name=[name].[ext]!./html/index.html');
-require('html/index.html');
+require('./html/index.html');
 require('./scss/base.scss');
 
 const angular = require('angular');
