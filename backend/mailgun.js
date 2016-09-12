26a107bc8d3a78db11fc1935404abf279703976e
Katherine Beame <katherine.beame@gmail.com>
2016-09-12 09:39:20 -0700

    express server

--- backend/mailgun.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
-
-const options = {
-  service: 'Mailgun'
-};
-
-module.exports = exports = nodemailer.createTransport(options);
