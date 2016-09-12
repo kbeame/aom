11e39a5e2a388fa927aa975c3d3485495ce75ea8
Katherine Beame <katherine.beame@gmail.com>
2016-09-12 10:21:15 -0700

    build

--- app/entry.js
+++ app/entry.js
@@ -11,7 +11,7 @@ require('./controller')(alphaApp);
 alphaApp.config(['$routeProvider', function($routing) {
   $routing
   .when('/home', {
-    templateUrl: 'templates/home_view.html',
+    templateUrl: './templates/home_view.html',
     controller: 'HomeController',
     controllerAs: 'homeCtrl'
   })
